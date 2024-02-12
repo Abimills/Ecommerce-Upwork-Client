2b173c4f0d031e423964367dc750a81c2baa9c16
abel <abelbeyene373@gmail.com>
2024-02-12 17:16:54 +0100

    added fetching od data to produts page

--- habesha-dress/src/app/components/Products/Products.tsx
+++ habesha-dress/src/app/components/Products/Products.tsx
@@ -1,13 +1,27 @@
+"use client";
+import axios from "axios";
 import ProductCard from "../ProductCard/ProductCard";
 import allProducts from "./allProducts.js";
+import { useEffect, useState } from "react";
 
 const Products: React.FC = () => {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const res = await axios.get("http://localhost:3000/api/product");
+    console.log(res.data);
+    setData(res.data.cloth);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <main className="w-full  h-full flex flex-col">
-      <h1 className="mb-16 mt-32 font-roboto font-md text-3xl  mx-4 ">Popular Products</h1>
+      <h1 className="mb-16 mt-32 font-roboto font-md text-3xl  mx-4 ">
+        Popular Products
+      </h1>
 
       <div className="w-full flex items-center gap-4 justify-between flex-wrap">
-        {allProducts.map((item: any) => {
+        {data?.map((item: any) => {
           return <ProductCard key={item.id} product={item} />;
         })}
       </div>
